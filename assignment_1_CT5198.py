3c8f7c9484d6c0719e30dca7f420a3164cfe4c6c
JayoH3001 <jasonpaulharney@gmail.com>
2025-01-23 16:25:03 +0000

    third commit

--- assignment_1_CT5198.py
+++ assignment_1_CT5198.py
@@ -8,29 +8,36 @@ customer_numbers = []
 for i in range(7):
     try:
         #convert input to int
-        userInput=int(input("Please enter customer count for Day" + str(i + 1) + ": "))
+        userInput=int(input("Please enter customer count for Day " + str(i + 1) + ": "))
+
+        if userInput < 0:  # Prevent negative customer counts
+            print("Customer count cannot be negative. Please restart the program..")
+            break #exits program
+
+        #add inputs to list
+        customer_numbers.append(userInput)
+        
     #check for correct input
     except ValueError:
-        print("Please only enter whole numbers")
-        break #breaks loop - requires program restart
-    #add inputs to list
-    customer_numbers.append(userInput)
+        print("Invalid Input. Please only enter whole numbers. Please restart the program.")
+        break
 
-#total over 7 days
-customer_count_7days = sum(customer_numbers)
-print("Total Number of Customers for 7 Days: ", customer_count_7days)
+if len(customer_numbers) == 7:
+    #total over 7 days
+    customer_count_7days = sum(customer_numbers)
+    print("Total Number of Customers for 7 Days: ", customer_count_7days)
 
-#avg over 7 days
-print("Average number of customers per day: ", int(customer_count_7days / len(customer_numbers)))
+    #avg over 7 days - rounded to next lower whole number as humans can't be counted with decimals
+    print("Average number of customers per day: ", int(customer_count_7days / len(customer_numbers)))
 
-#max number
-print("Maximum customer count: ", max(customer_numbers))
+    #max number
+    print("Maximum customer count: ", max(customer_numbers))
 
-#min number
-print("Minimum customer count: ", min(customer_numbers))
+    #min number
+    print("Minimum customer count: ", min(customer_numbers))
 
-#print list per day
-#print("List of customer counts per day: ",customer_numbers)
-print("Customer count by day: ")
-for day, count in enumerate(customer_numbers, start=1):
-    print("Day", day,": ", count, "customers")
+    #print list per day
+    #print("List of customer counts per day: ",customer_numbers)
+    print("Customer count by day: ")
+    for day, count in enumerate(customer_numbers, start=1):
+        print("Day", day,": ", count, "customers")
